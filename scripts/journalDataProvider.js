112184f42937041b55524f50d38860aa2f0c774f
buckleyderekp <buckleyderekp@gmail.com>
2020-03-02 15:46:38 -0600

    added ids to objects

--- scripts/journalDataProvider.js
+++ scripts/journalDataProvider.js
@@ -9,24 +9,28 @@
 // This is the original data. Can't Touch This.
 const journal = [
     {
+        id: 1,
         date: "07/24/2025",
-        concept: "HTML & CSS",
+        concept: "Test",
         entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
         mood: "Ecstatic"
     },
     {
+        id: 2,
         date: "02/27/2020",
         concept: "automating html rendering using javascript",
         entry: "we automated fish entry for martins aquarium using java script and it was confusing",
         mood: "Happy"
     },
     {
+        id: 3,
         date: "02/26/2020",
         concept: "hello world group project final",
         entry: "we played around with our hello world and tried key fram anumations",
         mood: "Indifferent"
     },
     {
+        id: 4,
         date: "02/28/2020",
         concept: "automating event listeners",
         entry: "automated event listeners for martins aquarium also went out as a group friday evening and met some people from other cohorts",
